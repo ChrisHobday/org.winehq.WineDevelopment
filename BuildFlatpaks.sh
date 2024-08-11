1bd620ff790c5133d64875770474df832a75e49b
Chris Hobday <Hobday.Chris@ProtonMail.com>
2024-08-11 10:14:49 -0400

    Update to 9.15, and add script that builds Flatpaks

---
+++ BuildFlatpaks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Build Flatpaks in repo directory
+flatpak run org.flatpak.Builder --force-clean --repo=repo build-dir org.winehq.WineDevelopment.yml
+
+# Build Flatpak bundles for WineDevelopment. WineDevelopment.mono and WineDevelopment.gecko
+flatpak build-bundle repo WineDevelopment9.15.flatpak org.winehq.WineDevelopment stable-23.08
+flatpak build-bundle --runtime repo WineDevelopment.mono9.2.0.flatpak org.winehq.WineDevelopment.mono stable-23.08
+flatpak build-bundle --runtime repo WineDevelopment.gecko2.47.4.flatpak org.winehq.WineDevelopment.gecko stable-23.08
